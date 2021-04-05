0ef612bb9224487e8c6ebae9fcd43429a2973d5d
yaoyu bao <yaobao5354@gmail.com>
2021-04-05 04:38:18 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
     t.integer "channel"
-  end
-
-  create_table "shows", force: :cascade do |t|
-    t.string "name"
   end
 
 end
